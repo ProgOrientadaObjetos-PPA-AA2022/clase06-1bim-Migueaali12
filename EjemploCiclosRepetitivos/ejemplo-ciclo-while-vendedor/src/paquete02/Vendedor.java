6072bf942e7abf4471bb5078042e07fcd5f67c68
Migueaali12 <malvarez11@utpl.edu.ec>
2022-05-19 21:50:28 -0500

    -aclase 06

--- EjemploCiclosRepetitivos/ejemplo-ciclo-while-vendedor/src/paquete02/Vendedor.java
+++ EjemploCiclosRepetitivos/ejemplo-ciclo-while-vendedor/src/paquete02/Vendedor.java
@@ -70,4 +70,23 @@ public class Vendedor {
     public double obtenerComisionPorAuto(){
         return comisionPorAuto;
     }
+    
+    @Override
+
+    public String toString() {
+
+        String g = String.format("Datos de Vendedor\n"
+                    + "Nombre: %s\n"
+                    + "Edad: %s\n"
+                    + "Salario mínimo: %.2f\n"
+                    + "Número de autos: %d\n"
+                    + "Pago mensual: %.2f\n", obtenerNombres(), 
+                    obtenerEdad(),
+                    obtenerSalarioMinimo(), 
+                    obtenerNumeroAutos(),
+                    obtenerPagoMensual());
+        return g;
+
+    }
+    
 }
--- EjemploCondicionales/EjemploPOOCondicionales/src/paqueteuno/Hospital.java
+++ EjemploCondicionales/EjemploPOOCondicionales/src/paqueteuno/Hospital.java
@@ -9,16 +9,16 @@ package paqueteuno;
  * @author reroes
  */
 public class Hospital {
-    
+
     private String nombreHospital;
     private String ciudadHospital;
     private int numeroCamas;
     private int numeroDoctores;
     private double presupuestoAnual;
-    
+
     // constructores 
     // Objetos para opción 1
-    public Hospital(String nombre, int camas, int doctores, double presupueto){
+    public Hospital(String nombre, int camas, int doctores, double presupueto) {
         nombreHospital = nombre;
         numeroCamas = camas;
         numeroDoctores = doctores;
@@ -26,74 +26,90 @@ public class Hospital {
         // para este tipo de hospitales la ciudad por defecto será Quito
         ciudadHospital = "Quito";
     }
-    
+
     // Objetos para opción 2
-    public Hospital(String nombre, int camas, int doctores){
+    public Hospital(String nombre, int camas, int doctores) {
         nombreHospital = nombre;
         numeroCamas = camas;
         numeroDoctores = doctores;
-        
+
         // para este tipo de hospitales la ciudad por defecto será Quito
         ciudadHospital = "Quito";
         // presupuesto anual será de 250 mil dolares con 10 centavos
         presupuestoAnual = 250000.10;
     }
-    
+
     // Objetos para opción 3
-    public Hospital(String nombre, String ciudad, int camas, int doctores, 
-            double presupueto){
+    public Hospital(String nombre, String ciudad, int camas, int doctores,
+            double presupueto) {
         nombreHospital = nombre;
         numeroCamas = camas;
         numeroDoctores = doctores;
         ciudadHospital = ciudad;
         presupuestoAnual = presupueto;
     }
+
     // métodos establecer para asignar valores a 
     // cada atributo
-    public void establecerNombreHospital(String n){
+    public void establecerNombreHospital(String n) {
         nombreHospital = n;
     }
-    
-    public void establecerCiudadHospital(String n){
+
+    public void establecerCiudadHospital(String n) {
         ciudadHospital = n;
     }
-    
-    public void establecerNumeroCamas(int n){
+
+    public void establecerNumeroCamas(int n) {
         numeroCamas = n;
     }
-    
-    public void establecerNumeroDoctores(int n){
+
+    public void establecerNumeroDoctores(int n) {
         numeroDoctores = n;
     }
-    
-    public void establecerPresupuestoAnual(double n){
+
+    public void establecerPresupuestoAnual(double n) {
         presupuestoAnual = n;
     }
-    
+
     // métodos obtener, que permitan acceder a los valores de
     // cada atributo
-    
-    public String obtenerNombreHospital(){
+    public String obtenerNombreHospital() {
         return nombreHospital;
     }
-    
-    public String obtenerCiudadHospital(){
+
+    public String obtenerCiudadHospital() {
         return ciudadHospital;
     }
-    
-    public int obtenerNumeroCamas(){
+
+    public int obtenerNumeroCamas() {
         return numeroCamas;
     }
-    
-    public int obtenerNumeroDoctores(){
+
+    public int obtenerNumeroDoctores() {
         return numeroDoctores;
     }
-    
-    public double obtenerPresupuestoAnual(){
+
+    public double obtenerPresupuestoAnual() {
         return presupuestoAnual;
     }
-    
-    
-    
-    
+
+    @Override
+
+    public String toString() {
+
+        String g = String.format("Datos Hospital%s\n"
+                + "Nombre del hospital: %s\n"
+                + "Ciudad del hospital: %s\n"
+                + "Número de camas: %d\n"
+                + "Número de doctores: %d\n"
+                + "Presupuesto anual: %.2f\n",
+                obtenerNombreHospital(),
+                obtenerCiudadHospital(),
+                obtenerNumeroCamas(),
+                obtenerNumeroDoctores(),
+                obtenerPresupuestoAnual());
+        return g;
+
+    }
+
 }
